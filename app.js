0cde56d91c43496732097bdf7dd70b5923c52994
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-24 20:03:23 +0100

    UL are now populated dynamically

--- app.js
+++ app.js
@@ -3,25 +3,20 @@ document.addEventListener('DOMContentLoaded', () => {
   const productsSection = document.querySelector('#products');
 
   for (let i = 0; i < data.products.length; ++i) {
-      const product = document.createElement('ul');
-      product.classList.add('product');
+      const product = data.products[i];
+      const productUL = document.createElement('ul');
+      productUL.classList.add('product');
 
-      const productName = document.createElement('li');
-      productName.textContent = data.products[i].name;
-      product.appendChild(productName);
-      productName.classList.add('product-name');
+      for (const property in product) {
+        const productProperty = document.createElement('li');
+        productProperty.textContent = product[property];
+        productUL.appendChild(productProperty);
+        // console.log(product[property]);
+      }
+      
+      // productName.classList.add('product-name');
+      productsSection.appendChild(productUL);
 
-      const productDescription = document.createElement('li');
-      productDescription.textContent = data.products[i].description;
-      product.appendChild(productDescription);
-
-      const productPrice = document.createElement('li');
-      productPrice.textContent = data.products[i].price;
-      product.appendChild(productPrice);
-
-      productsSection.appendChild(product);
-
-      console.log(productName.textContent);
     }
 
 });
