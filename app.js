d19a4b8a5b76cd99d4c06eae86c69eb7eb53fdb6
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-24 16:35:42 +0100

    3 different for loop alternatives because why not

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  const productsSection = document.querySelector('#products');
   const productsHTML = document.createElement('ul');
-  data.products.forEach(product => {
-    const productHTML = document.createElement('li');
-    productHTML.textContent = product.name;
-    productsHTML.appendChild(productHTML);
-  });
-  products.appendChild(productsHTML);
-  // const productsSection = document.querySelector('#products');
-  // console.log(productsSection);
+  
+// C-style "for" loop solution:
+    for (let i = 0; i < data.products.length; i++) {
+      const productHTML = document.createElement('li');
+      productHTML.textContent = data.products[i].name;
+      productsHTML.appendChild(productHTML);
+      console.log(productHTML.textContent);
+    }
+
+// JavaScript-style "for" loop solution:
+//   for (const product of data.products) {
+//     const productHTML = document.createElement('li');
+//     productHTML.textContent = product.name;
+//     productsHTML.appendChild(productHTML);
+//     console.log(product.name);
+//   }
+
+// Enumerator solution:
+//   data.products.forEach(product => {
+//     const productHTML = document.createElement('li');
+//     productHTML.textContent = product.name;
+//     productsHTML.appendChild(productHTML);
+//     console.log(product.name);
+//   });
+
+  productsSection.appendChild(productsHTML);
+
 });
