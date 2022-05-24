3975e294458980207792b7f3a813586a053f412a
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-05-24 20:41:13 +0100

    Minor indentation fix

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const productsSection = document.querySelector('#products');
 
   for (const product of data.products) {
-      const productUL = document.createElement('ul');
-      productUL.classList.add('product');
+    const productUL = document.createElement('ul');
+    productUL.classList.add('product');
 
-      for (const property in product) {
-        const propertyLI = document.createElement('li');
-        propertyLI.classList.add(`product-${property}`);
-        propertyLI.textContent = product[property];
-        productUL.appendChild(propertyLI);
-      }
+    for (const property in product) {
+      const propertyLI = document.createElement('li');
+      propertyLI.classList.add(`product-${property}`);
+      propertyLI.textContent = product[property];
+      productUL.appendChild(propertyLI);
+    }
 
-      productsSection.appendChild(productUL);
+    productsSection.appendChild(productUL);
   }
 });
